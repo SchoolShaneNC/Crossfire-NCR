256eba101ac1eaeeb0ed0ce53d99b5763c9fa2fd
MMarsh6 <mmarsh6@ncstudents.niagaracollege.ca>
2024-11-12 14:49:33 -0500

    submit button

--- ncr-crossfireUpdated/scripts/save-doc-popup.js
+++ ncr-crossfireUpdated/scripts/save-doc-popup.js
@@ -6,7 +6,7 @@ save.addEventListener("click", function(){
 
 document.getElementById("save-doc-button")
 .addEventListener("click", function(){
-    let test = document.createTextNode("Quality Inspector submitted a new Document")
+    let test = document.createTextNode("Saved a Document")
     let ul = document.getElementById("notification-list")
     let li = document.createElement("li")
 
@@ -15,3 +15,15 @@ document.getElementById("save-doc-button")
 
     document.getElementById("bell-icon").style.animation="bell-update 500ms linear 3"
 })
+
+document.getElementById("submit-doc-button")
+    .addEventListener("click", function(){
+        let test = document.createTextNode("Saved a Document")
+        let ul = document.getElementById("notification-list")
+        let li = document.createElement("li")
+
+        li.appendChild(test)
+        ul.appendChild(li)
+    
+        document.getElementById("bell-icon").style.animation="bell-update 500ms linear 3"
+    })
