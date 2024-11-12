677a5e57f04d42de8fd18c4fc68451f5be294d42
ConnorMcEnroeFoy <cfoy3@ncstudents.niagaracollege.ca>
2024-11-12 12:58:37 -0500

    Connor

--- ncr-crossfireUpdated/scripts/save-doc-popup.js
+++ ncr-crossfireUpdated/scripts/save-doc-popup.js
@@ -1,7 +1,17 @@
-var save = document.getElementById("save-doc-button");
-
-
+/*var save = document.getElementById("save-doc-button");
 
 save.addEventListener("click", function(){
-    alert("Document saved idiot");
+    alert("byee");
+})*/
+
+document.getElementById("save-doc-button")
+.addEventListener("click", function(){
+    let test = document.createTextNode("Quality Inspector submitted a new Document")
+    let ul = document.getElementById("notification-list")
+    let li = document.createElement("li")
+
+    li.appendChild(test)
+    ul.appendChild(li)
+
+    document.getElementById("bell-icon").style.animation="bell-update 500ms linear 3"
 })
