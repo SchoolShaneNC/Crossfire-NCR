b3d5408ae0abc8bb362d2335780bc45368e1f2ed
ConnorMcEnroeFoy <cfoy3@ncstudents.niagaracollege.ca>
2024-11-17 19:01:55 -0500

    Notifications message dialog
    -buttons have context appropriate message list for notifications
    
    -Removed duplicate list items in notifications scroll
    
    -Changed Engineer NCR layout

--- ncr-crossfireUpdated/scripts/save-doc-popup.js
+++ ncr-crossfireUpdated/scripts/save-doc-popup.js
@@ -1,11 +1,88 @@
-document.getElementById("save-doc-button")
+
+//Save notifications
+document.getElementById("qi-save-doc-button")
 .addEventListener("click", function(){
     let ncrNumber = document.getElementById("ncrNo").value
 
-    let test = document.createTextNode("NCR "+ncrNumber+" has been saved")
+    let test = document.createTextNode("Quality inspector saved NCR " + ncrNumber)
     let ul = document.getElementById("notification-list")
     let li = document.createElement("li")
 
     li.appendChild(test)
     ul.appendChild(li)
 })
+
+document.getElementById("engineer-save-doc-button")
+.addEventListener("click", function(){
+    let ncrNumber = document.getElementById("ncrNo").value
+
+    let test = document.createTextNode("Engineer saved NCR " + ncrNumber)
+    let ul = document.getElementById("notification-list")
+    let li = document.createElement("li")
+
+    li.appendChild(test)
+    ul.appendChild(li)
+})
+
+
+//Submit notifications
+document.getElementById("qi-submit-doc-button")
+.addEventListener("click", function(){
+    let ncrNumber = document.getElementById("ncrNo").value
+
+    let test = document.createTextNode("NCR " + ncrNumber + "Quality Inspector Section completed")
+    let ul = document.getElementById("notification-list")
+    let li = document.createElement("li")
+
+    li.appendChild(test)
+    ul.appendChild(li)
+})
+
+document.getElementById("engineer-submit-doc-button")
+.addEventListener("click", function(){
+    let ncrNumber = document.getElementById("ncrNo").value
+
+    let test = document.createTextNode("NCR " + ncrNumber + "Engineer Section completed")
+    let ul = document.getElementById("notification-list")
+    let li = document.createElement("li")
+
+    li.appendChild(test)
+    ul.appendChild(li)
+})
+
+
+//Edit notifications
+document.getElementById("engineer-change-doc-button")
+.addEventListener("click", function(){
+    let ncrNumber = document.getElementById("ncrNo").value
+
+    let test = document.createTextNode("NCR " + ncrNumber + "had changes made by Engineer")
+    let ul = document.getElementById("notification-list")
+    let li = document.createElement("li")
+
+    li.appendChild(test)
+    ul.appendChild(li)
+})
+
+document.getElementById("qi-change-doc-button")
+.addEventListener("click", function(){
+    let ncrNumber = document.getElementById("ncrNo").value
+
+    let test = document.createTextNode("NCR " + ncrNumber + "had changes made by Quality Inspector")
+    let ul = document.getElementById("notification-list")
+    let li = document.createElement("li")
+
+    li.appendChild(test)
+    ul.appendChild(li)
+})
+
+/*
+//Nofication local storage
+const notifications = []
+
+var ul
+li = document.getElementsByTagName("li")
+notifications.push(li)
+
+sessionStorage.setItem('notifData', notifications)
+*/
