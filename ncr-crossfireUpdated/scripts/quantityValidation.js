18cc9f9f4e0f26c2b08159c0c51573b82ef7f4ce
Lucky34433 <luckee5002@gmail.com>
2024-11-17 23:56:46 -0500

    Fixed Validation + Length Limits
    Added maxlength where needed and Validation now reset input to 0 if attempting to type anything below 0

--- ncr-crossfireUpdated/scripts/quantityValidation.js
+++ ncr-crossfireUpdated/scripts/quantityValidation.js
@@ -7,6 +7,12 @@ quantityReceived.addEventListener("input", () => {
     const receivedValue = parseInt(quantityReceived.value) || 0; // Current value of quantityReceived
     quantityDefective.max = receivedValue; // Updates max attribute of quantityDefective
 
+    // Make sure receivedValue is greater than 0
+    if (receivedValue < 0) {
+        quantityReceived.value = 0; // Resets to 0 if below 0
+        receivedValue = 0;
+    }
+
     // Ensure that quantityDefective is not higher than quantityReceived
     if (parseInt(quantityDefective.value) > receivedValue) {
         quantityDefective.value = receivedValue; // Resets Input if over max
