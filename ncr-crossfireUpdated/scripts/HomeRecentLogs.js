82a0dec986eab573bb7e7cbf2868ad396be061d6
SchoolShaneNC <smilton2@ncstudents.niagaracollege.ca>
2024-11-18 00:18:43 -0500

    re commit
    due do unfortunate events just had to redo this

---
+++ ncr-crossfireUpdated/scripts/HomeRecentLogs.js
@@ -0,0 +1,44 @@
+//getting the json data same old same old
+const data1 = localStorage.getItem('LoggedData');
+const data2 = JSON.parse(data1);
+let LogData = [];
+LogData = data2;
+let newData = [];
+console.log(LogData);
+//parsing ncr num so i can .sort() it then pushing to new array to slice bottom sorted 5 
+for(z of LogData.logs){
+    x = (parseInt(z.NcrNo.substring(0,4) + parseInt(z.NcrNo.substring(5))));
+newData.push(x);
+newData.sort()
+}
+const ndFilter = newData.slice(-5);
+//if the sliced array index = parsed ncr num then it displays that matching log. rest is same as viewlog page
+literalString = "";
+    for(let x = 0; x < LogData.logs.length; x++){
+        for(z of ndFilter){
+            if (z == (parseInt(LogData.logs[x].NcrNo.substring(0,4) + parseInt(LogData.logs[x].NcrNo.substring(5))))){
+    
+    var tblRow =    "<tr><td>" + LogData.logs[x].NcrNo + 
+                    "</td><td>" + LogData.logs[x].Dated + 
+                    "</td><td>" + LogData.logs[x].SupplierName +
+                    "</td><td>" + LogData.logs[x].Status;
+                    if (LogData.logs[x].Status == "Open"){
+                        tblRow += "</td><td><button class=" +'button' + " id=" +LogData.logs[x].NcrNo+" type=" +"button" + " " + "onclick = editNCR(this.value);" + "location.href="+"'formEdit.html'" + " value=" + LogData.logs[x].NcrNo +">Edit</button>"
+                        tblRow += "</td><td><button class=" +'button' + " id=" +LogData.logs[x].NcrNo+" type=" +"button" + " " + "onclick = editNCR(this.value);" + "location.href="+"'formViewFromLog.html'" + " value=" + LogData.logs[x].NcrNo +">View</button>"
+                        }else{
+                            tblRow += "</td><td><button class=" +'button' + " id=" +LogData.logs[x].NcrNo+" type=" +"button" + " " + "onclick = editError();" +  " value=" + LogData.logs[x].NcrNo +">Edit</button>"
+                            tblRow += "</td><td><button class=" +'button' + " id=" +LogData.logs[x].NcrNo+" type=" +"button" + " " + "onclick = editNCR(this.value);" + "location.href="+"'formViewFromLog.html'" + " value=" + LogData.logs[x].NcrNo +">View</button>"
+                        }
+                        tblRow +="</td></tr>" 
+                        literalString += tblRow;
+                        document.getElementById("ncr-body-recent").innerHTML = literalString;
+                    }
+}};
+//same from view logs page using same localstorage and edit and view scripts (edit2 edit3)
+function editNCR(value){
+    localStorage.setItem('editSelect', value);    
+}
+function editError(){
+    alert(`Cannot edit log because status is Closed`);
+}
+
