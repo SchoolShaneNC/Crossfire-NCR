4b6dab02fdf3fb98064ce1f828c5220e7d41b3cc
Zmilton1 <zmilton1@ncstudents.niagaracollege.ca>
2024-11-18 17:25:51 -0500

    added button close and create pdf
    added the close and create pdf button, "close" function still need to be made but the PDF is being created

---
+++ ncr-crossfireUpdated/scripts/closeandcreatepdf.js
@@ -0,0 +1,48 @@
+const {jsPDF } = window.jspdf;
+
+
+
+
+
+
+// Debugging: Check if the button exists
+document.getElementById("pdf").addEventListener("click", generatePDF);
+
+
+
+
+
+async function generatePDF(event) {
+    //Take all of the data getelementbyID inside this function
+    event.preventDefault(); 
+    console.log("Generate PDF function triggered"); 
+    
+    const formfromlog = document.getElementById('formViewFromLog');
+    
+    
+    const doc = new jsPDF({
+        orientation: "portrait", 
+        unit: "px", 
+        format: "a4", 
+    });
+
+    
+    const pageWidth = doc.internal.pageSize.getWidth(); 
+    const pageHeight = doc.internal.pageSize.getHeight(); 
+    const contentWidth = formfromlog.offsetWidth; 
+    const contentHeight = formfromlog.offsetHeight; 
+
+    const scaleFactor = Math.min(pageWidth / contentWidth, pageHeight / contentHeight); 
+
+    await doc.html(formfromlog, {
+        callback: function (doc) {
+            console.log("PDF generated"); 
+            doc.save(`NCR${NCRVAriable}.pdf`); 
+        },
+        x: 10, 
+        y: 10, 
+        html2canvas: {
+            scale: scaleFactor, 
+        },
+    });
+}
