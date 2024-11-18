d573d42d86ef913db308bde6a14bcc32e6a3e1e3
MMarsh6 <mmarsh6@ncstudents.niagaracollege.ca>
2024-11-18 16:55:47 -0500

    lining up filter for archive

--- ncr-crossfireUpdated/scripts/archiveLog.js
+++ ncr-crossfireUpdated/scripts/archiveLog.js
@@ -4,18 +4,15 @@ const jsonData = localStorage.getItem('LoggedData');
 const data = JSON.parse(jsonData);
 let logData = [];
 logData = data.logs;
-console.log(logData);
 
 let filteredLog = logData.filter(NCR => NCR.Status === "Closed");
-console.log(filteredLog);
 
 let literalString = "";
 
 $.each(filteredLog, function(i, ncr){
     var tblRow =    "<tr><td>" + ncr.NcrNo + 
-                    "</td><td>" + ncr.ProductNum + 
-                    "</td><td>" + ncr.QuantRec +
-                    "</td><td>" + ncr.QuantDef;
+                    "</td><td>" + ncr.Dated + 
+                    "</td><td>" + ncr.SupplierName;
     
                     
                     tblRow += "</td><td><button class=" +'button' + " id=" +ncr.NcrNo+" type=" +"button" + " " + "onclick = editNCR(this.value);" + "location.href="+"'formViewFromLog.html'" + " value=" + ncr.NcrNo +">View</button>"
