3979dbb3b879183c4e32c6a43249a99962c5975f
MMarsh6 <mmarsh6@ncstudents.niagaracollege.ca>
2024-12-04 16:55:43 -0500

    temporary turning emails off

--- ncr-crossfireUpdated/scripts/autoEmail.js
+++ ncr-crossfireUpdated/scripts/autoEmail.js
@@ -1,24 +1,24 @@
-console.log('firing script')
-export const QIEmail = (message, parmNCR, username) =>
-{
-    console.log('firing qiemail')
-    emailjs.init({
-        publicKey: 'YwDeaUeS7CNQTIjj7',
-      });
+
+// export const QIEmail = (message, parmNCR, username) =>
+// {
+//     console.log('firing qiemail')
+//     emailjs.init({
+//         publicKey: 'YwDeaUeS7CNQTIjj7',
+//       });
     
-      var templateParams = {
-        message: message,
-        ncr_number: parmNCR,
-        user_name: username,
-        user_email: "email@email.com"
-      };
+//       var templateParams = {
+//         message: message,
+//         ncr_number: parmNCR,
+//         user_name: username,
+//         user_email: "email@email.com"
+//       };
       
-      emailjs.send('Contact_Service', 'template_6891csa', templateParams).then(
-        (response) => {
-          console.log('SUCCESS!', response.status, response.text);
-        },
-        (error) => {
-          console.log('FAILED...', error);
-        },
-      );
-};
+//       emailjs.send('Contact_Service', 'template_6891csa', templateParams).then(
+//         (response) => {
+//           console.log('SUCCESS!', response.status, response.text);
+//         },
+//         (error) => {
+//           console.log('FAILED...', error);
+//         },
+//       );
+// };
