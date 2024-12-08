25691c60474f6278439327e9f3f49ea73df83e37
Zmilton1 <zmilton1@ncstudents.niagaracollege.ca>
2024-12-07 20:42:53 -0500

    updated accordionByRole to make it a switch
    reduces errors by removing if statements and implementing a switch statement

--- ncr-crossfireUpdated/scripts/accordionByRole.js
+++ ncr-crossfireUpdated/scripts/accordionByRole.js
@@ -1,25 +1,26 @@
-console.log(currentRole)
+console.log(currentRole);
 
-if (currentRole = "QI") {
-    document.getElementById('QIview').open = true;
-    document.getElementById('Engview').open = false;
-    document.getElementById('Purchview').open = false;
-}
-
-if (currentRole = "EN") {
-    document.getElementById('QIview').open = false;
-    document.getElementById('Engview').open = true;
-    document.getElementById('Purchview').open = false;
-}
-
-if (currentRole = "PA") {
-    document.getElementById('QIview').open = false;
-    document.getElementById('Engview').open = false;
-    document.getElementById('Purchview').open = true;
-}
-
-if (currentRole = "AD") {
-    document.getElementById('QIview').open = true;
-    document.getElementById('Engview').open = true;
-    document.getElementById('Purchview').open = true;
+switch (currentRole) {
+    case "QI":
+        document.getElementById('QIview').open = true;
+        document.getElementById('Engview').open = false;
+        document.getElementById('Purchview').open = false;
+        break;
+    case "EN":
+        document.getElementById('QIview').open = false;
+        document.getElementById('Engview').open = true;
+        document.getElementById('Purchview').open = false;
+        break;
+    case "PA":
+        document.getElementById('QIview').open = false;
+        document.getElementById('Engview').open = false;
+        document.getElementById('Purchview').open = true;
+        break;
+    case "AD":
+        document.getElementById('QIview').open = true;
+        document.getElementById('Engview').open = true;
+        document.getElementById('Purchview').open = true;
+        break;
+    default:
+        console.error("Invalid role: " + currentRole);
 }
